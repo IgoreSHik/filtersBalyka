007d19b558fb406891937b64e8399114604b8219
Ihar Balyka <balyko070699@gmail.com>
2021-05-16 17:38:46 +0200

    Добавьте файлы проекта.

---
+++ filtersBalyka/Utils/CustomFilterAttributes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace filtersBalyka.Utils
+{
+    public class CustomFilterAttributes : ResultFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            int a = 0;
+            var result = context.Result;
+            if (result is PageResult)
+            {
+                var page = ((PageResult)result);
+                page.ViewData["filterMessage"] = "Komunikat z CustomFilterAttributes!!!";
+            }
+        }
+    }
+}
